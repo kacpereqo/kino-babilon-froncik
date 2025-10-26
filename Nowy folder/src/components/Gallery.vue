1327150783ed177bf3208f0c889b439c9477086e
kacpereqo <remzak.k@gmail.com>
2025-10-26 22:50:36 +0100

    big ficzur

---
+++ Nowy folder/src/components/Gallery.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="gallery">
+    <h1>Galeria</h1>
+    <p>Witamy w galerii naszego serwisu!</p>
+    <div class="image-grid">
+      <img src="/galeria1.webp" />
+      <img src="/galeria2.webp" />
+      <img src="/galeria3.webp" />
+      <img src="/galeria4.webp" />
+      <img src="/galeria5.webp" />
+      <img src="/galeria6.webp" />
+      <img src="/galeria7.webp" />
+      <img src="/galeria8.webp" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped>
+img {
+  width: 500px;
+  height: 500px;
+}
+</style>
