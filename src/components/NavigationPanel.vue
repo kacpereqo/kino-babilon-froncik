184cf79a80e19adf62998457160e1c7358c59230
kacpereqo <remzak.k@gmail.com>
2025-10-28 20:28:26 +0100

    feat: router link

--- src/components/NavigationPanel.vue
+++ src/components/NavigationPanel.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/">Kalendarz</a></li>
-      <li><a href="/">Głosowanie</a></li>
-      <li><a href="/">Sex</a></li>
-      <li><a href="/galeria">Galeria</a></li>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/galeria">Kalendarz</router-link></li>
+      <li><router-link to="/galeria">Sex</router-link></li>
+      <li><router-link to="/galeria">Repertuar</router-link></li>
+      <li><router-link to="/galeria">Galeria</router-link></li>
     </ul>
   </div>
 </template>
